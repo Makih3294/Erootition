52d31c76935df5f1eaa7f54282f5e79c630d263b
none <none>
2021-08-05 16:43:29 -0400

    fixed the error

---
+++ climateChange.swift
@@ -0,0 +1,20 @@
+//
+//  climateChange.swift
+//  Erootition
+//
+//  Created by Scholar on 8/5/21.
+//
+
+import UIKit
+
+class climateChange: UIScrollView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
